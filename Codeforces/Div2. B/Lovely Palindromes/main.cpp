a56658020b95c700744539cdaa87b854f34b62bc
Amr Essam <47161400+A1Essam@users.noreply.github.com>
2019-10-16 20:23:56 +0200

    pset21

---
+++ Codeforces/Div2. B/Lovely Palindromes/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+
+
+    int sq ;
+    cin>>sq;
+    string colorseq="ROYGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIVGBIV";
+
+    for(int i = 0 ; i < sq;i++){
+        cout<<colorseq[i];
+    }
+
+    return 0;
+}
